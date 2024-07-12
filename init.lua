ea9e11fd0aed292b4119daafda0e3af2eeac06eb
Lauri Niskanen <ape@ape3000.com>
2024-07-12 16:48:10 +0300

    Use tabs for indentation

--- init.lua
+++ init.lua
@@ -1,53 +1,53 @@
 local function nlink()
-    local h = cx.active.current.hovered
-    if not h then
-        return ui.Span("")
-    end
+	local h = cx.active.current.hovered
+	if not h then
+		return ui.Span("")
+	end
 
-    if not h.cha.nlink then
-        return ui.Span("")
-    end
+	if not h.cha.nlink then
+		return ui.Span("")
+	end
 
-    return ui.Span(h.cha.nlink .. " ")
+	return ui.Span(h.cha.nlink .. " ")
 end
 
 local function owner()
-    local h = cx.active.current.hovered
-    if not h then
-        return ui.Span("")
-    end
+	local h = cx.active.current.hovered
+	if not h then
+		return ui.Span("")
+	end
 
-    local user = ya.user_name(h.cha.uid) or h.cha.uid
-    local group = ya.group_name(h.cha.gid) or h.cha.gid
+	local user = ya.user_name(h.cha.uid) or h.cha.uid
+	local group = ya.group_name(h.cha.gid) or h.cha.gid
 
-    return ui.Span(user .. ":" .. group .. " ")
+	return ui.Span(user .. ":" .. group .. " ")
 end
 
 local function mtime()
-    local h = cx.active.current.hovered
-    if not h then
-        return ui.Span("")
-    end
+	local h = cx.active.current.hovered
+	if not h then
+		return ui.Span("")
+	end
 
-    if not h.cha.modified then
-        return ui.Span("")
-    end
+	if not h.cha.modified then
+		return ui.Span("")
+	end
 
-    return ui.Span(os.date("%Y-%m-%d %H:%M", h.cha.modified // 1) .. " ")
+	return ui.Span(os.date("%Y-%m-%d %H:%M", h.cha.modified // 1) .. " ")
 end
 
 return {
-    setup = function(_, _)
-        Status.render = function(self, area)
-            self.area = area
+	setup = function(_, _)
+		Status.render = function(self, area)
+			self.area = area
 
-            local left = ui.Line { self:mode() }
-            local right = ui.Line { self:permissions(), ui.Span(" "), nlink(), owner(), mtime(), self:position() }
-            return {
-                ui.Paragraph(area, { left }),
-                ui.Paragraph(area, { right }):align(ui.Paragraph.RIGHT),
-                table.unpack(Progress:render(area, right:width())),
-            }
-        end
-    end,
+			local left = ui.Line({ self:mode() })
+			local right = ui.Line({ self:permissions(), ui.Span(" "), nlink(), owner(), mtime(), self:position() })
+			return {
+				ui.Paragraph(area, { left }),
+				ui.Paragraph(area, { right }):align(ui.Paragraph.RIGHT),
+				table.unpack(Progress:render(area, right:width())),
+			}
+		end
+	end,
 }
